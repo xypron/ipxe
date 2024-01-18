211cfeebab8e28250b6d554cb4cd64a9b2d30ee3
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2024-01-18 06:47:15 +0100

    riscv64: implement byte swap
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

---
+++ src/arch/riscv64/include/bits/byteswap.h
@@ -0,0 +1,37 @@
+#ifndef _BITS_BYTESWAP_H
+#define _BITS_BYTESWAP_H
+
+/** @file
+ *
+ * Byte-order swapping functions
+ */
+
+#include <stdint.h>
+
+FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
+
+uint16_t
+__bswap_variable_16 ( uint16_t x );
+
+static inline __attribute__ (( always_inline )) void
+__bswap_16s ( uint16_t *x ) {
+	*x = __bswap_variable_16 ( *x );
+}
+
+uint32_t
+__bswap_variable_32 ( uint32_t x );
+
+static inline __attribute__ (( always_inline )) void
+__bswap_32s ( uint32_t *x ) {
+	*x = __bswap_variable_32 ( *x );
+}
+
+uint64_t
+__bswap_variable_64 ( uint64_t x );
+
+static inline __attribute__ (( always_inline )) void
+__bswap_64s ( uint64_t *x ) {
+	*x = __bswap_variable_64 ( *x );
+}
+
+#endif
